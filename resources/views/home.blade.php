c8e0696ed61a528f6d6552c6c04269dd5cabfc83
Pedro <pedro@thefuzzyfish.com>
2019-04-21 01:18:04 -0300

    Adding home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6 mb-5">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Plástico</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,67 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <img src="{{ asset('/svg/plastico.svg') }}">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 mb-5">
+            <div class="card">
+                <div class="card-header">Cartón</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <img src="{{ asset('/svg/carton.svg') }}">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 mb-5">
+            <div class="card">
+                <div class="card-header">Papel</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <img src="{{ asset('/svg/papel.svg') }}">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 mb-5">
+            <div class="card">
+                <div class="card-header">Metal</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <img src="{{ asset('/svg/metal.svg') }}">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">Vidrio</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <img src="{{ asset('/svg/vidrio.svg') }}">
                 </div>
             </div>
         </div>
